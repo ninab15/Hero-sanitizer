91358e34dae2fd5943ea1f279960fed789318011
Ninab15 <nina1501@live.dk>
2020-10-13 12:06:26 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
+window.onscroll = function () {
+    scrollIndicator()
+};
+
+function scrollIndicator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 
 gsap.from(".heading", {
     opacity: 0,
